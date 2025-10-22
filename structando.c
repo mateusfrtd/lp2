730cae20103d7460b6882516d526351a01d244a4
mateusfrtd <mateusaiinfreitasapelao@gmail.com>
2025-10-21 22:33:20 -0300

    coisas

--- structando.c
+++ structando.c
@@ -17,13 +17,12 @@ struct Paciente{
 
 void showMenu(void)
 {
-	printf("Escolha a opção:\n1- Cadastrar um novo paciente\n2- Mostrar pacientes cadastrados\n3- Remover paciente cadastrado\n0- Sair\n");	
+	printf("\n----------------------------\nEscolha a opção:\n1- Cadastrar um novo paciente\n2- Mostrar pacientes cadastrados\n3- Remover paciente cadastrado\n0- Sair\n----------------------------\n");	
 
 };
 
 void _register(struct Paciente *pacientes, int idx)
 {	
-	int ah;
 	printf("Nome: ");
 	fgets(pacientes[idx].nome, 40, stdin);
 	pacientes[idx].nome[strcspn(pacientes[idx].nome, "\n")] = '\0';
@@ -35,9 +34,8 @@ void _register(struct Paciente *pacientes, int idx)
 	printf("Telefone: ");
 	fgets(pacientes[idx].tel, 11, stdin);
 	pacientes[idx].tel[strcspn(pacientes[idx].tel, "\n")] = '\0';
-	scanf("%d", &ah);
 
-	printf("Adicionado\nID: %d\n%s - %d/%d/%d\n   Tel: %s\n", idx, pacientes[idx].nome, pacientes[idx].nasc.dia, pacientes[idx].nasc.mes, pacientes[idx].nasc.ano, pacientes[idx].tel);
+	printf("\n----------------------------\nAdicionado\nID: %d\n%s - %d/%d/%d\n   Tel: %s\n\n----------------------------\n", idx, pacientes[idx].nome, pacientes[idx].nasc.dia, pacientes[idx].nasc.mes, pacientes[idx].nasc.ano, pacientes[idx].tel);
 	getchar();
 };
 
@@ -49,25 +47,25 @@ void delete(struct Paciente *pacientes)
 	getchar();
 	char name[40];
 	strcpy(name, pacientes[p].nome);
-	strcpy(pacientes[p].nome, '\0');
-	strcpy(pacientes[p].tel, '\0');
+	pacientes[p].nome[0] = '\0';
+	pacientes[p].tel[0] = '\0';
 	pacientes[p].nasc.dia = 0;
 	pacientes[p].nasc.mes = 0;
 	pacientes[p].nasc.ano = 0;
-	printf("O paciente %s foi removido.\n", name);
+	printf("\n----------------------------\nO paciente %s foi removido.\n----------------------------\n", name);
 };
 	
 void showPatients(struct Paciente *pacientes, int idx)
 {
 	for(int j=0; j<idx; j++){
-		printf("ID: %d\n%s - %d/%d/%d\n   Tel: %s", j, pacientes[j].nome, pacientes[j].nasc.dia, pacientes[j].nasc.mes, pacientes[j].nasc.ano, pacientes[j].tel);
+		printf("\n----------------------------\nID: %d\n%s - %d/%d/%d\n   Tel: %s\n----------------------------\n", j, pacientes[j].nome, pacientes[j].nasc.dia, pacientes[j].nasc.mes, pacientes[j].nasc.ano, pacientes[j].tel);
 	}
 };
 
 int main(void)
 {
 	struct Paciente pacientes[100];
-	int op, idx;
+	int op, idx, c;
 	idx = 0;
 
 	while(1){
@@ -78,6 +76,8 @@ int main(void)
 		switch (op){
 			case 1:
 				_register(pacientes, idx);
+			while ((c = getchar()) != '\n' && c != EOF);
+				idx++;
 				break;
 			case 2:
 				showPatients(pacientes, idx);
@@ -85,15 +85,12 @@ int main(void)
 			case 3:
 				delete(pacientes);
 				break;
-			case 4:
-				printf("Encerrando");
+			case 0:
+				printf("Encerrando\n");
 				return 0;
 			default:
-				printf("default\n");
+				printf("Opção invalida.\n");
 		}
-
-	idx++;
 	}
-
 	return 0;
 };
